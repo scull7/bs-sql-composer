6c8f50e87290775610d4ca5a64009b1de3d9bf1d
Nathan Sculli <scull7@gmail.com>
2018-03-01 22:36:39 -0800

    Basic handling of select statements.

--- __tests__/SqlComposerTest.ml
+++ __tests__/SqlComposerTest.ml
@@ -4,10 +4,11 @@ let () =
 
 describe "Test SqlComposer Interface" (fun () ->
   test "A simple query" (fun () ->
-    let expected = "SELECT * FROM test  WHERE 1=1" in
-    let actual = SqlComposer.Query.(
-      from "test"
-      |> Pipe.select "*"
+    let expected = "SELECT\n  *\nFROM test\nWHERE 1=1" in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "*"
       |> to_sql
     )
     in
@@ -15,12 +16,13 @@ describe "Test SqlComposer Interface" (fun () ->
   );
   test "A query with a join" (fun () ->
     let expected =
-      "SELECT * FROM test JOIN foo WHERE 1=1"
+      "SELECT\n  *\nFROM test\nJOIN foo\nWHERE 1=1"
     in
-    let actual = SqlComposer.Query.(
-      from "test"
-      |> Pipe.select "*"
-      |> Pipe.join "JOIN foo"
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "*"
+      |> join "JOIN foo"
       |> to_sql
     )
     in
@@ -28,12 +30,13 @@ describe "Test SqlComposer Interface" (fun () ->
   );
   test "A query with a where clause" (fun () ->
     let expected =
-      "SELECT * FROM test  WHERE 1=1 AND test.foo = ?"
+      "SELECT\n  *\nFROM test\nWHERE 1=1\nAND test.foo = ?"
     in
-    let actual = SqlComposer.Query.(
-      from "test"
-      |> Pipe.select "*"
-      |> Pipe.where "AND test.foo = ?"
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "*"
+      |> where "AND test.foo = ?"
       |> to_sql
     )
     in
@@ -41,19 +44,229 @@ describe "Test SqlComposer Interface" (fun () ->
   );
   test "Adding to a base query" (fun () ->
     let expected =
-      "SELECT foo, bar FROM test  WHERE 1=1 AND test.foo = ?"
+      "SELECT\n  foo\n  , bar\nFROM test\nWHERE 1=1\nAND test.foo = ?"
     in
-    let base_query = SqlComposer.Query.(
-      from "test"
-      |> Pipe.select "foo"
-      |> Pipe.select "bar"
+    let base_query = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "foo"
+      |> field "bar"
     )
     in
-    let actual = SqlComposer.Query.(
-      where base_query "AND test.foo = ?"
+    let actual = SqlComposer.Select.(
+      where "AND test.foo = ?" base_query
       |> to_sql
     )
     in
     Expect.expect actual |> Expect.toBe expected
-  )
+  );
+  test "Field alias" (fun () ->
+    let expected =
+      "SELECT\n  foo AS bar\nFROM test\nWHERE 1=1"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "foo AS bar"
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Limit" (fun () ->
+    let expected =
+      "SELECT\n  foo AS bar\nFROM test\nWHERE 1=1\nLIMIT 10"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "foo AS bar"
+      |> limit ~row_count:10
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Limit with offset" (fun () ->
+    let expected =
+      "SELECT\n  foo AS bar\nFROM test\nWHERE 1=1\nLIMIT 10 OFFSET 2"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "foo AS bar"
+      |> limit ~row_count:10 ~offset:2
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Order By DESC" (fun () ->
+    let expected =
+      "SELECT
+  foo AS bar
+FROM test
+WHERE 1=1
+ORDER BY
+  foo DESC
+LIMIT 10 OFFSET 2"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> order_by (`Desc "foo")
+      |> field "foo AS bar"
+      |> limit ~row_count:10 ~offset:2
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Order By ASC" (fun () ->
+    let expected =
+      "SELECT
+  foo AS bar
+FROM test
+WHERE 1=1
+ORDER BY
+  foo ASC
+LIMIT 10 OFFSET 2"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> order_by (`Asc "foo")
+      |> field "foo AS bar"
+      |> limit ~row_count:10 ~offset:2
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Order By 2 fields" (fun () ->
+    let expected =
+      "SELECT
+  foo AS bar
+FROM test
+WHERE 1=1
+ORDER BY
+  foo ASC
+, bar DESC
+LIMIT 10 OFFSET 2"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> order_by (`Asc "foo")
+      |> order_by (`Desc "bar")
+      |> field "foo AS bar"
+      |> limit ~row_count:10 ~offset:2
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Adding a distinct modifier" (fun () ->
+    let expected = "SELECT DISTINCT\n  *\nFROM test\nWHERE 1=1" in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "*"
+      |> modifier `Distinct
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Adding a STRAIGHT_JOIN modifier" (fun () ->
+    let expected = "SELECT STRAIGHT_JOIN\n  *\nFROM test\nWHERE 1=1" in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "*"
+      |> modifier `StraightJoin
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Adding a SQL_NO_CACHE modifier" (fun () ->
+    let expected = "SELECT SQL_NO_CACHE\n  *\nFROM test\nWHERE 1=1" in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "*"
+      |> modifier `MySqlNoCache
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Adding a SQL_CALC_FOUND_ROWS modifier" (fun () ->
+    let expected = "SELECT SQL_CALC_FOUND_ROWS\n  *\nFROM test\nWHERE 1=1" in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> field "*"
+      |> modifier `MySqlCalcFoundRows
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Adding a distinct and high priority modifier" (fun () ->
+    let expected = "SELECT DISTINCT HIGH_PRIORITY\n  *\nFROM test\nWHERE 1=1" in
+    let actual = SqlComposer.Select.(
+      select
+      |> modifier `Distinct
+      |> from "test"
+      |> field "*"
+      |> modifier `HighPriority
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Group By" (fun () ->
+    let expected =
+"SELECT DISTINCT
+  foo
+FROM test
+WHERE 1=1
+GROUP BY
+  foo"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> group_by "foo"
+      |> field "foo"
+      |> modifier `Distinct
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
+  test "Group By multiple" (fun () ->
+    let expected =
+"SELECT DISTINCT
+  foo
+FROM test
+WHERE 1=1
+GROUP BY
+  foo
+, bar"
+    in
+    let actual = SqlComposer.Select.(
+      select
+      |> from "test"
+      |> group_by "foo"
+      |> field "foo"
+      |> modifier `Distinct
+      |> group_by "bar"
+      |> to_sql
+    )
+    in
+    Expect.expect actual |> Expect.toBe expected
+  );
 );
--- src/SqlComposer.ml
+++ src/SqlComposer.ml
@@ -1,49 +1,96 @@
 
-module Query = struct
+module Select = struct
   type t = {
-    select: string list;
+    modifier: string option;
+    fields: string list;
     from: string list;
     join: string list;
     where: string list;
+    group_by: string list;
+    order_by: string list;
+    limit: string list;
   }
 
-  (* @TODO add order_by *)
-  (* @TODO add group_by *)
-  (* @TODO add limit *)
-
-  let from table = {
-    select = [];
-    from = [ table; "FROM" ];
+  let select = {
+    modifier = None;
+    fields = [];
+    from = [];
     join = [];
     where = [ "WHERE 1=1" ];
+    group_by = [];
+    order_by = [];
+    limit = [];
   }
 
-  let select query statement = {
-    query with select = statement :: query.select
+  let modifier flag query =
+    let modifier = match flag with
+      | `Distinct -> "DISTINCT"
+      | `HighPriority -> "HIGH_PRIORITY"
+      | `StraightJoin -> "STRAIGHT_JOIN"
+      | `MySqlNoCache -> "SQL_NO_CACHE"
+      | `MySqlCalcFoundRows -> "SQL_CALC_FOUND_ROWS"
+    in
+    let setting = match query.modifier with
+      | Some s -> String.concat " " [ s; modifier ]
+      | None -> modifier
+    in
+    { query with modifier = Js.Option.some setting }
+
+  let field statement query =
+    let compiled = if Belt_List.length query.fields > 0
+      then {j|  , $statement|j}
+      else {j|  $statement|j}
+    in
+    { query with fields = compiled :: query.fields}
+
+  let from table query = {
+    query with from = [ {j|FROM $table|j}]
   }
 
-  let join query statement = {
+  let join statement query = {
     query with join = statement :: query.join
   }
 
-  let where query statement = {
+  let where statement query = {
     query with where = statement :: query.where
   }
 
-  module Pipe = struct
-    let select statement query = select query statement
-    let join statement query = join query statement
-    let where statement query = where query statement
-  end
+  let group_by statement query =
+    let group_by = if Belt_List.length query.group_by < 1
+      then [ {j|  $statement|j}; "GROUP BY" ]
+      else {j|, $statement|j} :: query.group_by
+    in
+    { query with group_by = group_by }
 
-  let to_sql { select; from; join; where } =
+  let order_by field query =
+    let clause = match field with
+      | `Asc f -> {j|$f ASC|j}
+      | `Desc f -> {j|$f DESC|j}
+    in
+    let order_by = if Belt_List.length query.order_by < 1
+      then [ {j|  $clause|j}; "ORDER BY" ]
+      else {j|, $clause|j} :: query.order_by
+    in
+    { query with order_by = order_by }
+
+  let limit ?offset ~row_count query =
+    let statement = match offset with
+      | Some x -> {j|LIMIT $row_count OFFSET $x|j}
+      | None -> {j|LIMIT $row_count|j}
+    in
+   { query with limit = [ statement ] }
+
+  let to_sql { modifier; fields; from; join; where; order_by; group_by; limit } =
+    let clause = match modifier with
+      | Some v -> {j|SELECT $v|j}
+      | None -> "SELECT"
+    in
+    let statements = [ fields; from; join; where; order_by; group_by; limit ] in
     let reverse_join sep statements =
       String.concat sep (Belt_List.reverse statements)
     in
-      String.concat " " [
-        (String.concat " " [ "SELECT"; (reverse_join ", " select) ]);
-        (reverse_join " " from);
-        (reverse_join " " join);
-        (reverse_join " " where);
-      ]
+    Belt_List.keep statements (fun x -> Belt_List.length x > 0)
+    |> (fun x -> [ clause ] :: x)
+    |> (fun x -> Belt_List.map x (reverse_join "\n"))
+    |> String.concat "\n"
 end
